0dd8a385507ea23e49b7a97ed43d15d2652c0d75
Krislv4nk <kristinalev4enko89@gmail.com>
2024-04-13 19:26:14 +0200

    auth operations, auth selectors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-// import WelcomePage from 'pages/WelcomePage/WelcomePage';
-import AuthPages from 'pages/AuthPages/AuthPages';
+import WelcomePage from 'pages/WelcomePage/WelcomePage';
+
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<AuthPages />} />
+        <Route index element={< WelcomePage/>} />
       </Route>
     </Routes>
   );
