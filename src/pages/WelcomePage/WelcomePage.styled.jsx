d4d72282b0ccce8d0d3568e4e5a63bca714e411d
OksanaDrobakha <6249784@gmail.com>
2024-04-14 22:54:48 +0400

    welcome page

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -10,15 +10,24 @@ export const WelcomeContainer = styled.div`
   padding: 0 20px 40px 20px;
   margin: 0 auto;
   background-image: url(${backgroundMobile});
+  background-size: cover;
   background-size: 100%;
   background-position: center bottom;
   background-repeat: no-repeat;
+
+  @media screen and (max-width: 767px) and (-webkit-min-device-pixel-ratio: 2),
+    (max-width: 767px) and (min-resolution: 192dpi) {
+  }
 
   @media screen and (min-width: 768px) and (max-width: 1439px) {
     width: 768px;
     padding: 0 32px 50px 32px;
     min-height: calc(100vh - 88px);
     background-image: url(${backgroundTablet});
+  }
+
+  @media screen and (min-width: 768px) and (-webkit-min-device-pixel-ratio: 2),
+    (min-width: 768px) and (min-resolution: 192dpi) {
   }
 
   @media screen and (min-width: 1440px) {
