6c53950c730a7f5383e7c80f521add0c6037dbf4
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-04-15 10:06:44 +0200

    add private public routes

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -4,7 +4,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://watertracker-backand-codekartel.onrender.com';
 
 const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWFkNDZlYWU0ZWIzNjYyZjZlMDg5NSIsImlhdCI6MTcxMzAzNDM4NSwiZXhwIjoxNzEzMTE3MTg1fQ.bZXFSnvQOsg9cGAWkdPSAJxrm1vewYo2RgnF6qa_A_Y';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWJhZmRlY2U3NmYyMzhkOGNhZTMyZCIsImlhdCI6MTcxMzE2NDI1NiwiZXhwIjoxNzEzMjQ3MDU2fQ.tOWsV5ScRNaqiBxgQE83J5H3hfaIFMcpzFO5Ucrx3pI';
 axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
 const getMonthPercentage = async date => {
