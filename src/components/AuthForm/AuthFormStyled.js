c29b9fb45920fa411a490aa8b843af866dd78aed
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-04-17 14:01:11 +0200

    fixed icons

--- src/components/AuthForm/AuthFormStyled.js
+++ src/components/AuthForm/AuthFormStyled.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { Form, Field, ErrorMessage } from 'formik';
 
-
-
 export const StyledForm = styled(Form)`
 display: flex;
 flex-direction: column;
@@ -17,38 +15,35 @@ gap: 16px;
     width: 384px;
   }
   
-`
+`;
 export const InputWrapper = styled.div`
-display: flex;
-flex-direction: column;
-gap: 8px;
-position: relative;
-z-index: 1;
-@media screen and (min-width: 1440px) {
-  
-}
-
-`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  position: relative;
+  z-index: 1;
+  @media screen and (min-width: 1440px) {
+  }
+`;
 export const StaledLabel = styled.label`
-font-size: 18px;
-line-height: 1.33;
-color: var(--primary-color-black);
-
-`
+  font-size: 18px;
+  line-height: 1.33;
+  color: var(--primary-color-black);
+`;
 export const StyledField = styled(Field)`
-border: 1px solid var(--secondary-color-blue-3);
-border-radius: 6px;
-padding: 12px 10px;
-width: 280px;
-height: 44px;
-color: var(--primary-color-blue);
-background: var(--primary-color-white); 
-&::placeholder {
+  border: 1px solid var(--secondary-color-blue-3);
+  border-radius: 6px;
+  padding: 12px 10px;
+  width: 280px;
+  height: 44px;
+  color: var(--primary-color-blue);
+  background: var(--primary-color-white);
+  &::placeholder {
     line-height: 1.25;
     color: var(--secondary-color-blue-2);
   }
-&:focus {
-    border-color: var(--secondary-color-blue-1); 
+  &:focus {
+    border-color: var(--secondary-color-blue-1);
   }
   &:hover {
     border-color: var(--secondary-color-blue-1);
@@ -67,63 +62,66 @@ background: var(--primary-color-white);
   @media screen and (min-width: 1440px) {
     width: 384px;
   }
-  
-`
+`;
 export const StyledSpan = styled.span`
-display: none;`
-
+  display: none;
+`;
 
 export const SvgIcon = styled.svg`
-fill: var(--primary-color-blue);
-width: 16px;
+  fill: transparent;
+  stroke: var(--primary-color-blue);
+  width: 16px;
   height: 16px;
-position: absolute;
-top: 60%;
+  position: absolute;
+  top: 60%;
   right: 14px;
   cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
   @media screen and (min-width: 768px) {
-left: 310px;
+    left: 310px;
   }
-  @media screen and (min-width: 1440px){
+  @media screen and (min-width: 1440px) {
     left: 355px;
   }
-`
- 
+`;
+
 export const StyledErrorMessage = styled(ErrorMessage)`
-font-size: 12px;
-line-height: 1.25;
-color: var(--secondary-color-red);
-`
-export const Button= styled.button`
-border-radius: 10px;
-padding: 8px 30px;
-width: 280px;
-height: 36px;
-font-weight: 500;
-font-size: 16px;
-line-height: 1.25;
-text-align: center;
-display: flex;
+  font-size: 12px;
+  line-height: 1.25;
+  color: var(--secondary-color-red);
+`;
+export const Button = styled.button`
+  border-radius: 10px;
+  padding: 8px 30px;
+  width: 280px;
+  height: 36px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 1.25;
+  text-align: center;
+  display: flex;
   justify-content: center;
   align-items: center;
-color: var(--primary-color-white);
-box-shadow: 0 4px 8px 0 rgba(64, 123, 255, 0.34);
-background: var(--primary-color-blue);
-margin-bottom: 16px;
+  color: var(--primary-color-white);
+  box-shadow: 0 4px 8px 0 rgba(64, 123, 255, 0.34);
+  background: var(--primary-color-blue);
+  margin-bottom: 16px;
 
-@media screen and (min-width: 768px) {
-  padding: 10px 30px;
-  width: 336px;
-  height: 44px;
-  font-size: 18px;
-line-height: 1.33;
-z-index: 2;
-}
-@media screen and (min-width: 1440px) {
-  width: 384px;
-}
+  @media screen and (min-width: 768px) {
+    padding: 10px 30px;
+    width: 336px;
+    height: 44px;
+    font-size: 18px;
+    line-height: 1.33;
+    z-index: 2;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 384px;
+  }
+`;
 
-`
+export const TestWrapper = styled.div`
+  position: relative;
+`;
--- src/pages/AuthPages/SignInPage.jsx
+++ src/pages/AuthPages/SignInPage.jsx
@@ -1,33 +1,42 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {  Navigate } from 'react-router-dom';
-import {AuthForm} from 'components/AuthForm/AuthForm';
+import { Navigate } from 'react-router-dom';
+import { AuthForm } from 'components/AuthForm/AuthForm';
 import { signinUser } from '../../redux/auth/operations';
 import { WrapperContainer } from 'components/Container/ContainerStyled';
-import {  AuthHeder, StyledLink, FirstBgImageWrapper, SecondBgImageWrapper} from "./AuthPagesStyled"
-
-
-
+import {
+  AuthHeder,
+  StyledLink,
+  FirstBgImageWrapper,
+  SecondBgImageWrapper,
+} from './AuthPagesStyled';
 
 const SignInPage = () => {
   const dispatch = useDispatch();
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const handleSubmit = (formData) => {
+  const handleSubmit = formData => {
     dispatch(signinUser(formData))
       .then(() => {
         setIsSignedIn(true);
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   };
   if (isSignedIn) {
     return <Navigate to="/homepage" />;
   }
-      
-  return( <FirstBgImageWrapper ><WrapperContainer><AuthHeder>Sign in</AuthHeder><AuthForm onSubmit={handleSubmit}/>
-  <StyledLink to="/signup">Sign up</StyledLink><SecondBgImageWrapper />
-  </WrapperContainer></FirstBgImageWrapper >);
+
+  return (
+    <FirstBgImageWrapper>
+      <WrapperContainer>
+        <AuthHeder>Sign in</AuthHeder>
+        <AuthForm onSubmit={handleSubmit} />
+        <StyledLink to="/signup">Sign up</StyledLink>
+        {/* <SecondBgImageWrapper /> */}
+      </WrapperContainer>
+    </FirstBgImageWrapper>
+  );
 };
 
 export default SignInPage;
