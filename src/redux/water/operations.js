dd0a895524093658b1fbef79b5a575b4c50b13cd
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-04-17 17:22:28 +0200

    water path

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -4,11 +4,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://watertracker-backand-codekartel.onrender.com';
 
 const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWZhOTc4NDRjZjc2NThiM2NhMTk0NyIsImlhdCI6MTcxMzM1MTA0MSwiZXhwIjoxNzEzNDMzODQxfQ.tPBqWbD4OVO73plDndGJRlzSEO7_CWKPupZfKGu7t-0';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWZhOTc4NDRjZjc2NThiM2NhMTk0NyIsImlhdCI6MTcxMzM2NzI2NCwiZXhwIjoxNzEzNDUwMDY0fQ.7Sdhg_7kwQk2GFaSouyOl8JD2MJfHlGGR6sYConLLtQ';
 axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
 const getMonthPercentage = async date => {
-  const { data } = await axios.get(`/month/?date=${date}`);
+  const { data } = await axios.get(`water/month/?date=${date}`);
   return data;
 };
 
