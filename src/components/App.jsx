49bb624ddc2589896969dd51a43a11e8f4423021
OksanaDrobakha <6249784@gmail.com>
2024-04-17 23:00:05 +0400

    favicon and page name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,21 @@ import SharedLayout from './SharedLayout/SharedLayout';
 import WelcomePage from 'pages/WelcomePage/WelcomePage';
 import HomePage from 'pages/HomePage/HomePage';
 import ModalTest from './Modal/ModalTest';
+import { Helmet } from 'react-helmet';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<WelcomePage />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/modal" element={<ModalTest />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>Tracker of water</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<WelcomePage />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/modal" element={<ModalTest />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
