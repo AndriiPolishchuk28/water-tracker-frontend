d1f266ed5ec59c5afdffac9c8a944301cc452c60
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2024-04-25 01:33:44 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,12 @@
-const Loader = () => {
-  return <div>Loader</div>;
-};
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+function Loader() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <CircularProgress />
+    </div>
+  );
+}
 
 export default Loader;
